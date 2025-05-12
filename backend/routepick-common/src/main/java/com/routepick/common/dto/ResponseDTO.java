a43279ece553dac846d7cccbbfc9746828add7af
손창욱 <changwook@sonchang-ug-ui-MacBookPro.local>
2025-05-12 23:22:11 +0900

    dto

---
+++ backend/routepick-common/src/main/java/com/routepick/common/dto/ResponseDTO.java
@@ -0,0 +1,11 @@
+package com.routepick.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ResponseDTO {
+
+    private String message;
+}
